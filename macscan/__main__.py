96ce74b93cf86f5fec94c5b0ac587697c7365b08
Fabien LOISON <flo@flogisoft.com>
2024-02-23 16:48:35 +0100

    feat(cli): initial CLI scan command (note: output param is currently ignored)

--- macscan/__main__.py
+++ macscan/__main__.py
@@ -1,7 +1,23 @@
+import os
 import sys
 import argparse
+from functools import partial
 
 from .browser import list_scanner_devices
+from .scanner import scan_document
+from .exceptions import DeviceUnavailableError
+
+
+def _type_path(mode, string):
+    if os.path.isfile(string):
+        if os.access(string, mode):
+            return string
+        raise argparse.ArgumentTypeError("can't access '%s'" % string)
+    else:
+        path = os.path.dirname(os.path.abspath(string))
+        if os.access(path, mode):
+            return string
+        raise argparse.ArgumentTypeError("the '%s' folder does not exist" % path)
 
 
 def print_scanner_devices():
@@ -22,6 +38,15 @@ def print_scanner_devices():
     print("%i scanner(s) found." % len(devices))
 
 
+def scan_to_file(parsed_args):
+    print("Scanning to '%s'..." % parsed_args.output)
+    try:
+        scan_document(parsed_args.output)
+    except DeviceUnavailableError as error:
+        print("E: %s" % str(error))
+        sys.exit(1)
+
+
 def main(args=sys.argv[1:]):
     parser = argparse.ArgumentParser(
         prog="macscan",
@@ -34,14 +59,25 @@ def main(args=sys.argv[1:]):
         help="list available scanner devices",
     )
 
+    scan_parser = subparsers.add_parser(
+        "scan",
+        prog="macscan scan",
+        help="scan a document to a file",
+    )
+    scan_parser.add_argument(
+        "output",
+        help="Output file path",
+        type=partial(_type_path, os.W_OK),
+    )
+
     parsed_args = parser.parse_args(args if args else ["--help"])
 
     if parsed_args.subcommand == "list":
         print_scanner_devices()
         sys.exit(0)
-    else:
-        print("E: Unknown command '%s'." % parsed_args.subcommand)
-        sys.exit(1)
+    elif parsed_args.subcommand == "scan":
+        scan_to_file(parsed_args)
+        sys.exit(0)
 
 
 if __name__ == "__main__":
